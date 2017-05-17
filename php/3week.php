bbf15bb6361af31ffa198d788397d5d3435f8a3f
untaka0822 <untaka0822@gmail.com>
2017-05-18 01:49:44 +0800

    完成

--- php/3week.php
+++ php/3week.php
@@ -18,39 +18,40 @@
   // membersテーブルからログインしているユーザーのデータを取得
   $sql = 'SELECT * FROM `members` WHERE `member_id`=?';
   $data = array($_SESSION['login_member_id']);
-  $stmt = $dbh->prepare($sql);
-  $stmt->execute($data);
-  $members = $stmt->fetch(PDO::FETCH_ASSOC);
+  $stmt2 = $dbh->prepare($sql);
+  $stmt2->execute($data);
+  $members = $stmt2->fetch(PDO::FETCH_ASSOC);
 
-  // // ページング機能
-  // $page = '';
-  // // パラメータのページ番号を取得
-  // if (isset($_REQUEST['page'])) {
-  //     $page = $_REQUEST['page'];
-  // }
+  // ページング機能
+  $page = '';
+  // パラメータのページ番号を取得
+  if (isset($_REQUEST['page'])) {
+      $page = $_REQUEST['page'];
+  }
 
-  // // パラメータが存在しない場合はページ番号を1とする
-  // if ($page == '') {
-  //     $page = 1;
-  // }
+  // パラメータが存在しない場合はページ番号を1とする
+  if ($page == '') {
+      $page = 1;
+  }
 
-  // // 1以下のイレギュラーな数値が入ってきた場合はページ番号を1とする
-  // $page = max($page, 1); // max ()の中に入った一番大きい数を出力 例: echo max(1, 10, 15, 1.9, 12.5) . '<br>'; の場合 15　が出力される
+  // 1以下のイレギュラーな数値が入ってきた場合はページ番号を1とする
+  $page = max($page, 1); // max ()の中に入った一番大きい数を出力 例: echo max(1, 10, 15, 1.9, 12.5) . '<br>'; の場合 15　が出力される
 
-  // // データの件数から最大ページ数を計算する
-  // $sql = 'SELECT COUNT(*) AS `cnt` FROM `diary`'; // 復習: tweetsテーブルからデータ全件の件数を数えて取得
-  // $data = array();
-  // $diary_stmt = $dbh->prepare($sql);
-  // $diary_stmt->execute();
-  // $diarys = $diary_stmt->fetch(PDO::FETCH_ASSOC);
-  // $max_page = ceil($diarys['cnt'] / 5); // ceil 小数点以下を切り上げ　(php 小数点　関数でggr)
+  // データの件数から最大ページ数を計算する
+  $sql = 'SELECT COUNT(*) AS `cnt` FROM `diary`'; // 復習: tweetsテーブルからデータ全件の件数を数えて取得
+  $data = array();
+  $diary_stmt = $dbh->prepare($sql);
+  $diary_stmt->execute();
+  $diarys = $diary_stmt->fetch(PDO::FETCH_ASSOC);
+  $max_page = ceil($diarys['cnt'] / 5); // ceil 小数点以下を切り上げ　(php 小数点　関数でggr)
 
-  // // パラメータのページ番号が最大ページ数を超えていれば、最後のページ数とする
-  // $page = min($page, $max_page); // min 最小値を返す
+  // パラメータのページ番号が最大ページ数を超えていれば、最後のページ数とする
+  $page = min($page, $max_page); // min 最小値を返す
 
-  // // 1ページに表示する件数分だけデータを取得する
-  // $page = ceil($page);
-  // $start = ($page - 1) * 5;
+  // 1ページに表示する件数分だけデータを取得する
+  $page = ceil($page);
+  // echo '現在のページ数 : ' . $page;
+  $start = ($page - 1) * 5;
 
   // 削除ボタンを押したとき
   if (!empty($_POST && $_POST['submit-type'] == 'delete')) {
@@ -67,13 +68,15 @@
       $_SESSION['join'] = $_POST;
       header('Location: edit.php');
       exit();
+
     }
 
-  // ログインしているユーザの日記を全件表示
-  $sql = 'SELECT * FROM `diary` WHERE `user_id`=?';
-  $data = array($_SESSION['login_member_id']);
-  $stmt = $dbh->prepare($sql);
-  $stmt->execute($data);
+    // ログインしているユーザの日記を全件表示
+    // $sql = sprintf('SELECT t.*, m.nick_name, m.picture_path FROM `tweets` t LEFT JOIN `members` m ON t.member_id=m.member_id ORDER BY t.created DESC LIMIT %d, 5', $start); // sprintf 引数に指定した値を指定の形式にフォーマットした文字列を取得 %d 整数値
+    $sql = sprintf('SELECT * FROM `diary` WHERE `user_id`=? ORDER BY diary_id DESC LIMIT %d, 5', $start);
+    $data = array($_SESSION['login_member_id']);
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($data);
 
 ?>
 
@@ -100,14 +103,14 @@
       <!-- ログインしているユーザーの日記を全件表示 -->
       <?php while($diary = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
         <div class="diary">
-            <a href="#"><?php echo $diary['title']; ?></a><br>
-            <p class="date"><?php echo $diary['created']; ?></p>
-            <form name="form3" method="POST" action="" onsubmit="return submitChk()"> <!-- onsubmitでダイアログの表示 -->
+              <a href="detail_diary.php"><?php echo $diary['title']; ?></a><br>
+              <p class="date"><?php echo $diary['created']; ?></p>
+            <form name="form2" method="POST" action="" onsubmit="return submitChk()"> <!-- onsubmitでダイアログの表示 -->
               <input class="btn-xs btn-info" type="submit" name="delete" value="削除" style="margin-bottom: 10px;">
               <input type="hidden" name="diary_id" value="<?php echo $diary['diary_id']; ?>">
               <input type="hidden" name="submit-type" value="delete">
             </form>
-            <form name="form1" method="POST" action="">
+            <form name="form3" method="POST" action="">
               <input class="btn-xs btn-success" type="submit" name="edit" value="編集" style="margin-bottom: 10px;">
               <input type="hidden" name="submit-type" value="edit">
               <input type="hidden" name="diary_id" value="<?php echo $diary['diary_id']; ?>">
@@ -123,14 +126,34 @@
       ?>
       <?php endwhile; ?>
       <!-- ページング機能必要か？？？ -->
+      <ul class="paging">
+          <?php
+            $word = '';
+            if (isset($_GET['search_word'])) {
+                $word = '&search_word=' . $_GET['search_word'];
+            }
+          ?>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <?php if($page > 1): ?>
+                <button style="background-color: yellow"><a href="3week.php?page=<?php echo $page - 1; ?><?php echo $word; ?>">前</a></button>
+            <?php else: ?>
+                <button>前</button>
+            <?php endif; ?>
+
+            &nbsp;&nbsp;|&nbsp;&nbsp;
+            <?php if($page < $max_page - 1): ?>
+                <button style="background-color: yellow"><a href="3week.php?page=<?php echo $page + 1; ?><?php echo $word; ?>">次</a></button>
+            <?php else: ?>
+                <button>次</button>
+            <?php endif; ?>
+      </ul>
      </div>
     </div>
   </div>
 
   <div id="c-box" style="text-align: center">
     <div class="content2">
-    <img src="../member_picture/<?php echo $members['picture_path']; ?>" style="width: 100%;
-  height: 64%;">
+    <img src="../member_picture/<?php echo $members['picture_path']; ?>" style="width: 100%; height: 64%; border-radius: 5px">
       <?php
           date_default_timezone_set('Asia/Tokyo'); // 時間を日本に設定
           $time = intval(date('H'));
@@ -191,7 +214,7 @@
     */
     function submitChk () {
         /* 確認ダイアログ表示 */
-        var flag = confirm ( "本当に削除してもいいですか？いいんですね？削除しちゃいますよ？\n\n削除したくない場合はボタンを押す前に今年の抱負を叫んでからキャンセル を押して下さい");
+        var flag = confirm ( "本当に削除してもいいですか？いいんですね？削除しちゃいますよ？\n\n削除したくない場合はボタンを押す前に今年の抱負を叫んでからキャンセルを押して下さい");
         /* send_flg が TRUEなら送信、FALSEなら送信しない */
         return flag;
     }
